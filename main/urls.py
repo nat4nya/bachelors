9f4ebbab18b4ce52f6a1906f064dc30c52c2684a
nat4nya.0@gmail.com <nat4nya.0@gmail.com>
2024-05-23 22:14:29 +0300

    different homepages for each group added

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('main', views.main, name='main'),
     path('home', views.home, name = 'home'),
+    path('home-student', views.home_student, name = "home_student"),
+    path("home-professor", views.home_professor, name = "home_professor"),
+    path('home-secretary', views.home_secretary, name = "home_secretary"),
     path('sign-up', views.sign_up, name='sign_up'),
     path("logout/", views.log_out, name="logout"),
+    path("create-post", views.create_post, name = "create_post")
 ]
--- main/views.py
+++ main/views.py
@@ -1,18 +1,40 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
-from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required, permission_required
+from .forms import RegisterForm, PostForm
+from .models import Post
 
 # Create your views here.
 
 @login_required(login_url = "/login")
-
 def home(request):
-    return render(request, 'main/home.html')
+    posts = Post.objects.all()
+    if request.method == "POST":
+        post_id = request.POST.get("post-id")
+        post = Post.objects.filter(id = post_id).first()
+        if post and post.author == request.user:
+            post.delete()
+
+    return render(request, 'main/home.html', {"posts": posts})
 
 def main(request):
     return render(request, 'main/main.html')
 
+
+
+@login_required(login_url='/login')
+def home_student(request):
+    return render(request, "main/home_student.html")
+
+@login_required(login_url='/login')
+def home_professor(request):
+    return render(request, "main/home_professor.html")
+
+@login_required(login_url='/login')
+def home_secretary(request):
+    return render(request, "main/home_secretary.html")
+
+@login_required(login_url = "/login")
 def log_out(request):
     logout(request)
     return redirect("/login/")
@@ -23,8 +45,24 @@ def sign_up(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('/main')
+            return redirect('/home')
     else:
         form = RegisterForm()
 
     return render(request, 'registration/sign_up.html', {"form": form})
+
+@login_required(login_url = "/login")
+@permission_required("main.add_post", login_url="/login", raise_exception=True)
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.save()
+            return redirect("/home")
+    else:
+        form = PostForm()
+
+    return render(request, 'main/create_post.html', {"form": form})
+
